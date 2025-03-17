07308f1d670aca8d717e121acea36d0298796f82
eWarzywo <mateusz2borach4@gmail.com>
2025-03-17 14:15:45 +0100

    Revert "Nabijanie linijek dla ematiego"
    This reverts commit a039004179b966f436a691b12039086384b5b9e9, reversing
    changes made to 4bae92360e5cb363fe991dfa35ff8e0154ba2893.
    
    revert ostatniego merge'a

--- forttask/src/app/layout.tsx
+++ forttask/src/app/layout.tsx
@@ -1,24 +1,30 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "../components/header";
+import Footer from "@/components/footer";
+import { Inter } from "next/font/google";
 
 const inter = Inter({
-    subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: 'FortTask',
-    description: 'Emati pisz kod',
+  title: "FortTask",
+  description: "Emati pisz kod",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className={inter.className}>
-            <body className="bg-[#09090B] text-[#FAFAFA] p-10 space-y-10 w-full h-screen">{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" className={inter.className}>
+      <body className="bg-[#09090B] text-[#FAFAFA] p-10 space-y-10 w-full h-screen">
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
