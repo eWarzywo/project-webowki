3a158b834e23fc04cf7296d0a50f9a3c24145b33
Majdanka <majdan_k@onet.pl>
2025-03-17 20:08:59 +0100

    Folder structure

--- forttask/src/app/layout.tsx
+++ forttask/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "../components/header";
-import Footer from "@/components/footer";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -21,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="bg-[#09090B] text-[#FAFAFA] py-6 px-10 space-y-10 w-full h-screen">
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
