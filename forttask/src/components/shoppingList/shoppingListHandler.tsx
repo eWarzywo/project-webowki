9b334b0d09668746191d0bb0e1a3cd583c2a2dce
Majdanka <majdan_k@onet.pl>
2025-04-16 10:26:43 +0200

    Pagination

--- forttask/src/components/shoppingList/shoppingListHandler.tsx
+++ forttask/src/components/shoppingList/shoppingListHandler.tsx
@@ -1,3 +1,16 @@
+import Pagination from '@/components/generalUI/pagination';
+
 export default function ShoppingListHandler() {
-    return <div className="w-5/6"></div>;
+    function getData() {
+        // Fetch data from the server or local storage
+        return ['meow', 'meow', 'meow'];
+    }
+
+    const data = getData();
+
+    return (
+        <div className="w-5/6 flex flex-[1_0_0] flex-col items-start rounded-xl border bg-zinc-950 border-zinc-800 text-zinc-50">
+            <Pagination data={data} itemsPerPage={1} />
+        </div>
+    );
 }
