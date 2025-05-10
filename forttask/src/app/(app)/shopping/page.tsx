ed4d8309f1a1f86bac6195afd349d19357614f6d
Majdanka <majdan_k@onet.pl>
2025-05-10 14:08:11 +0200

    bills form pt1

--- forttask/src/app/(app)/shopping/page.tsx
+++ forttask/src/app/(app)/shopping/page.tsx
@@ -17,7 +17,7 @@ export default function Shopping() {
 
         setErrorMessage('');
         if (name.trim().length < 3) {
-            setErrorMessage('Name must be at least 3 characters long');
+            setErrorMessage('Name must be at least 3 characters');
             return;
         }
         if (isNaN(cost) || cost <= 0) {
@@ -74,13 +74,13 @@ export default function Shopping() {
                     onSubmit={handleSubmit}
                     className="gap-0 self-start w-1/6 flex flex-col items-center rounded-xl border border-zinc-800 bg-zinc-950 max-h-[400px]"
                 >
-                    <div className="flex p-6 flex-col items-start justify-center">
+                    <div className="flex p-6 flex-col items-start justify-start">
                         <h3 className="text-zinc-50 flex text-2xl font-semibold">Add new item</h3>
                         <h4 className="text-zinc-400 mt-1.5 text-sm">Add a new item to your shopping list</h4>
                     </div>
                     <div className="px-6 pb-6 space-y-4 flex flex-col items-start">
                         <div className="flex flex-col items-start justify-start w-full gap-2.5 mt-1.5">
-                            <label className="text-zinc-50 text-sm" htmlFor="name">
+                            <label className="text-zinc-50 text-sm px-1" htmlFor="name">
                                 Name
                             </label>
                             <input
@@ -94,7 +94,7 @@ export default function Shopping() {
                             />
                         </div>
                         <div className="flex flex-col items-start justify-start w-full mt-1.5 gap-2.5 ">
-                            <label className="text-zinc-50 text-sm" htmlFor="cost">
+                            <label className="text-zinc-50 text-sm px-1" htmlFor="cost">
                                 Cost
                             </label>
                             <div
@@ -115,21 +115,21 @@ export default function Shopping() {
                                 <span className="text-zinc-400">$</span>
                             </div>
                         </div>
-                    </div>
-                    <div className="w-full gap-2.5 flex justify-between px-9 pb-6 items-center">
-                        <input
-                            type="reset"
-                            value="Cancel"
-                            className="border rounded-xl gap-2.5 px-6 py-2 border-zinc-800 flex max-h-10 min-h-10 flex-col justify-center items-center hover:bg-zinc-800 hover:border-zinc-400 text-zinc-50 font-medium text-sm cursor-pointer"
-                        />
-                        <input
-                            type="submit"
-                            value="Add"
-                            className="bg-zinc-50 text-zinc-900 px-6 py-2 rounded-xl gap-2.5 hover:bg-zinc-600 hover:text-zinc-200 hover:border hover:border-zinc-200 cursor-pointer text-sm font-medium"
-                        />
-                    </div>
-                    <div id="error" className="text-red-500 text-sm px-6 pb-4">
-                        {errorMessage}
+                        <div className=" flex w-full gap-[10px] justify-between items-center">
+                            <input
+                                type="reset"
+                                value="Cancel"
+                                className="border rounded-xl gap-2.5 px-6 py-2 border-zinc-800 flex max-h-10 min-h-10 flex-col justify-center items-center hover:bg-zinc-800 hover:border-zinc-400 text-zinc-50 font-medium text-sm cursor-pointer"
+                            />
+                            <input
+                                type="submit"
+                                value="Add"
+                                className="bg-zinc-50 text-zinc-900 px-6 py-2 rounded-xl gap-2.5 hover:bg-zinc-600 hover:text-zinc-200 hover:border hover:border-zinc-200 cursor-pointer text-sm font-medium"
+                            />
+                        </div>
+                        <div id="error" className="text-red-500 text-sm">
+                            {errorMessage}
+                        </div>
                     </div>
                 </form>
                 {!addedToggle && <ShoppingListHandler />}
